ade4886ae0c423a0888316fab4f248b93e78ef43
fzramos <64625115+fzramos@users.noreply.github.com>
2021-08-12 17:22:30 -0500

    Completed single input interactive chart

--- user_input_callbacks.py
+++ user_input_callbacks.py
@@ -24,18 +24,11 @@ airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.a
 df = airline_data.sample(n=500, random_state=0)
 
 # Creating Charts for overall dataset
-fig3 = px.pie(df, values='Flights', names='DistanceGroup', 
+fig1 = px.pie(df, values='Flights', names='DistanceGroup', 
              title='Distance group proportion by flights')
-fig4 = px.bar(df, x='Flights', y='DistanceGroup', orientation='h',
+fig2 = px.bar(df, x='Flights', y='DistanceGroup', orientation='h',
              title='Flights per Distance group')
 
-# Creating initial state of interactive charts
-# Ideally I would like to not do this, all this is basically the function
-# Hopefully Dash will be samrt enough to see the inital state of the button is at 2014 and use the 
-# callbakc function to build this chart, just not sure how itll like figure=fig1 to be empty initially
-default_year=2014
-
-# fig1.show()
 # Initializing app
 app=dash.Dash(__name__)
 
@@ -44,7 +37,7 @@ app.layout = html.Div(children=[html.H1('Interactive Dashboard'),
                                 html.Div([
                                     html.H2('Interactive Graph 1'),
                                     html.Div([
-                                        'Input:', dcc.Input(id='input-yr-1', value=2014),
+                                        'Input:', dcc.Input(id='input-yr-1', value='2018'),
                                     ]),
                                     html.Br(),
                                     html.Br(),
@@ -53,31 +46,31 @@ app.layout = html.Div(children=[html.H1('Interactive Dashboard'),
                                 html.Div([
                                     html.H2('Interactive Graph 2'),
                                     html.Div([
-                                        'Input:', dcc.Input(id='input-yr-2', value=2014),
+                                        'Input:', dcc.Input(id='input-yr-2', value='2018'),
                                     ]),
                                     html.Br(),
                                     html.Br(),
                                     html.Div(dcc.Graph(id='line-plot-1'))
                                 ]),
-                                dcc.Graph(figure=fig3),
-                                dcc.Graph(figure=fig4)
+                                dcc.Graph(figure=fig1),
+                                dcc.Graph(figure=fig2)
                                 ])
 # note: can't just add id to dcc components
 
 @app.callback(Output(component_id='bar-plot-1', component_property='figure'),
                 Input(component_id='input-yr-1', component_property='value'))
 def graph_top10_yr(yr):
-    df_yr = df[df['Year']==yr]
+    df_yr = df[df['Year']==int(yr)]
     df_yr_top_airlines = df_yr.groupby('Reporting_Airline')['Flights'].sum().nlargest(10).reset_index()
     return px.bar(df_yr_top_airlines, y='Reporting_Airline', x='Flights', orientation='h')
 
 @app.callback(Output(component_id='line-plot-1', component_property='figure'),
                 Input(component_id='input-yr-2', component_property='value'))
 def month_avg_delay_yr(yr):
-    df_yr = df[df['Year']==yr]
+    df_yr = df[df['Year']==int(yr)]
     df_avg_delay_month = df_yr.groupby('Month')['DepDelay'].mean().reset_index()
-    return px.scatter(df_avg_delay_month, x='Month', y='DepDelay')
+    return px.line(df_avg_delay_month, x='Month', y='DepDelay')
 
 if __name__=='__main__':
-    app.run_server(port=8053, host='localhost', debug=True)
+    app.run_server(port=8053, host='localhost')
 
