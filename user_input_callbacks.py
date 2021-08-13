74cd00eb81b158b46e06c2626649af7beda03718
fzramos <64625115+fzramos@users.noreply.github.com>
2021-08-13 00:31:31 -0500

    Fully functional multi-input interactive chart

--- user_input_callbacks.py
+++ user_input_callbacks.py
@@ -21,7 +21,8 @@ airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.a
                                     'Div2Airport': str,
                                     'Div2TailNum': str})
 
-df = airline_data.sample(n=500, random_state=0)
+# df = airline_data.sample(n=500, random_state=0)
+df = airline_data
 
 # Creating Charts for overall dataset
 fig1 = px.pie(df, values='Flights', names='DistanceGroup', 
@@ -37,7 +38,7 @@ app.layout = html.Div(children=[html.H1('Interactive Dashboard'),
                                 html.Div([
                                     html.H2('Interactive Graph 1'),
                                     html.Div([
-                                        'Input:', dcc.Input(id='input-yr-1', value='2018'),
+                                        'Year:', dcc.Input(id='input-yr-1', value='2018'),
                                     ]),
                                     html.Br(),
                                     html.Br(),
@@ -46,11 +47,21 @@ app.layout = html.Div(children=[html.H1('Interactive Dashboard'),
                                 html.Div([
                                     html.H2('Interactive Graph 2'),
                                     html.Div([
-                                        'Input:', dcc.Input(id='input-yr-2', value='2018'),
+                                        'Year:', dcc.Input(id='input-yr-2', value='2018'),
                                     ]),
                                     html.Br(),
                                     html.Br(),
                                     html.Div(dcc.Graph(id='line-plot-1'))
+                                ]),
+                                html.Div([
+                                    html.H2('Interactive Graph 3 (Multi-Input'),
+                                    html.Div([
+                                        'Year:', dcc.Input(id='input-yr-3', value='2018'),
+                                        '\tDestination State Abbr.:', dcc.Input(id='input-symbol-1', value='AZ')
+                                    ]),
+                                    html.Br(),
+                                    html.Br(),
+                                    html.Div(dcc.Graph(id='bar-plot-2'))
                                 ]),
                                 dcc.Graph(figure=fig1),
                                 dcc.Graph(figure=fig2)
@@ -71,6 +82,18 @@ def month_avg_delay_yr(yr):
     df_avg_delay_month = df_yr.groupby('Month')['DepDelay'].mean().reset_index()
     return px.line(df_avg_delay_month, x='Month', y='DepDelay')
 
+# Multiple Input Callback
+@app.callback(Output(component_id='bar-plot-2', component_property='figure'),
+                [
+                    Input(component_id='input-yr-3', component_property='value'),
+                    Input(component_id='input-symbol-1', component_property='value')
+                ])
+def graph_top10_yr_state(yr, state):
+    df_yr = df.loc[(df['Year']==int(yr)) & (df['DestState']==state.upper())]
+    df_yr_top_airlines = df_yr.groupby('Reporting_Airline')['Flights'].sum().nlargest(10).reset_index()
+    print(df_yr_top_airlines)
+    return px.bar(df_yr_top_airlines, y='Reporting_Airline', x='Flights', orientation='h')
+
 if __name__=='__main__':
-    app.run_server(port=8053, host='localhost')
+    app.run_server(port=8054, host='localhost')
 
