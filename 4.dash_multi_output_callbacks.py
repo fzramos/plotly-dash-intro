072713fb1977b5c013ca6975cd875233d985ec39
fzramos <64625115+fzramos@users.noreply.github.com>
2021-08-13 20:57:22 -0500

    dash app skeleton and some of callback function working

---
+++ 4.dash_multi_output_callbacks.py
@@ -0,0 +1,72 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+from dash.dependencies import Input, Output
+
+# Get dataset and get sample
+airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+                            encoding = 'ISO-8859-1',
+                            dtype={'Div1Airport': str,
+                                    'Div1TailNum': str,
+                                    'Div2Airport': str,
+                                    'Div2TailNum': str})
+print(airline_data.shape)
+df = airline_data.sample(n=1500, random_state=0)
+# df = airline_data
+
+
+# Creating Charts for overall dataset
+# fig1 = px.pie(df, values='Flights', names='DistanceGroup', 
+#              title='Distance group proportion by flights')
+# fig2 = px.bar(df, x='Flights', y='DistanceGroup', orientation='h',
+#              title='Flights per Distance group')
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[html.H1('Flight Delay Time Statistics',
+                                style={'textAlign':'right'}),
+                                html.Div(children=["Input Year:", 
+                                        dcc.Input(id='in-yr', value='2011')
+                                ]),
+                                dcc.Graph(id='chart-1'),
+                                dcc.Graph(id='chart-2'),
+                                dcc.Graph(id='chart-3'),
+                                dcc.Graph(id='chart-4'),
+                                dcc.Graph(id='chart-5'),
+
+                                ])
+
+@app.callback(
+    [
+        Output(component_id='chart-1', component_property='figure'),
+        Output(component_id='chart-2', component_property='figure'),
+        Output(component_id='chart-3', component_property='figure'),
+        Output(component_id='chart-4', component_property='figure'),
+        Output(component_id='chart-5', component_property='figure'),
+    ],
+    Input(component_id='in-yr', component_property='value')
+)
+def create_charts_for_yr(yr):
+    df_yr = df.loc[df['Year']==int(yr)]
+    df_carrier_yr_delays = df_yr.groupby(['Reporting_Airline', 'Month'], as_index=False)\
+        [['DepDelayMinutes','WeatherDelay','ArrDelay','NASDelay','SecurityDelay','LateAircraftDelay']]\
+        .mean()
+    fig1 = px.line(df_carrier_yr_delays, 
+                        x='Month',
+                        y='DepDelayMinutes',
+                        color='Reporting_Airline')
+
+    fig2 = px.line(df_carrier_yr_delays, 
+                        x='Month',
+                        y='WeatherDelay',
+                        color='Reporting_Airline')
+
+    # return [fig1, fig, fig3, fig4, fig5]
+    return [fig1, fig2, fig1, fig1, fig1]
+
+
+if __name__ == "__main__":
+    app.run_server()
