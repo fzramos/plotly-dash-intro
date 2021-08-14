d0d3dfaaa3d4183d2fca5117caa2e6e3afd010bf
fzramos <64625115+fzramos@users.noreply.github.com>
2021-08-13 21:33:52 -0500

    Completed interactive, multi-graph dashboard

--- 4.dash_multi_output_callbacks.py
+++ 4.dash_multi_output_callbacks.py
@@ -7,28 +7,27 @@ import plotly.express as px
 from dash.dependencies import Input, Output
 
 # Get dataset and get sample
-airline_data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
                             encoding = 'ISO-8859-1',
                             dtype={'Div1Airport': str,
                                     'Div1TailNum': str,
                                     'Div2Airport': str,
                                     'Div2TailNum': str})
-df = airline_data.sample(n=2500, random_state=0)
-
-# Creating Charts for overall dataset
-# fig1 = px.pie(df, values='Flights', names='DistanceGroup', 
-#              title='Distance group proportion by flights')
-# fig2 = px.bar(df, x='Flights', y='DistanceGroup', orientation='h',
-#              title='Flights per Distance group')
-
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[html.H1('Flight Delay Time Statistics',
-                                style={'textAlign':'right'}),
+                                style={
+                                    'textAlign':'center',
+                                    'font-size': 30,
+                                    'color': '#503D36'
+                                }),
                                 html.Div(children=["Input Year:", 
-                                        dcc.Input(id='in-yr', value='2011'),
-                                        ], style={'font-size': 30}
+                                        dcc.Input(id='in-yr', value='2011', type='number'),
+                                        ], style={
+                                            'font-size': 30,
+                                            'height':'35px',
+                                            'font-size':'30'}
                                 ),
                                 html.Br(),
                                 html.Br(),
@@ -55,32 +54,34 @@ app.layout = html.Div(children=[html.H1('Flight Delay Time Statistics',
 )
 def create_charts_for_yr(yr):
     df_yr = df.loc[df['Year']==int(yr)]
-    df_carrier_yr_delays = df_yr.groupby(['Reporting_Airline', 'Month'], as_index=False)\
-        [['DepDelayMinutes','WeatherDelay','ArrDelay','NASDelay','SecurityDelay','LateAircraftDelay']]\
-        .mean()
-    # need to convert all non-minute columns from hours to minutes
-    df_carrier_yr_delays[['WeatherDelay','ArrDelay','NASDelay','SecurityDelay','LateAircraftDelay']] = \
-        df_carrier_yr_delays[['WeatherDelay','ArrDelay','NASDelay','SecurityDelay','LateAircraftDelay']] * 60
+    df_carrier_yr_delays = df_yr.groupby(['Month', 'Reporting_Airline'], as_index=False)\
+        [['CarrierDelay','WeatherDelay','NASDelay','SecurityDelay','LateAircraftDelay']]\
+        .mean().reset_index()
     fig1 = px.line(df_carrier_yr_delays, 
                         x='Month',
-                        y='DepDelayMinutes',
-                        color='Reporting_Airline')
+                        y='CarrierDelay',
+                        color='Reporting_Airline',
+                        title='Average Carrier Delay by Month per Airline')
     fig2 = px.line(df_carrier_yr_delays, 
                         x='Month',
                         y='WeatherDelay',
-                        color='Reporting_Airline')
+                        color='Reporting_Airline',
+                        title='Average Weather Delay by Month per Airline')
     fig3 = px.line(df_carrier_yr_delays, 
                         x='Month',
                         y='NASDelay',
-                        color='Reporting_Airline')
+                        color='Reporting_Airline',
+                        title='Average NAS Delay by Month per Airline')
     fig4 = px.line(df_carrier_yr_delays, 
                         x='Month',
                         y='SecurityDelay',
-                        color='Reporting_Airline')  
+                        color='Reporting_Airline',
+                        title='Average Security Delay by Month per Airline') 
     fig5 = px.line(df_carrier_yr_delays, 
                         x='Month',
                         y='LateAircraftDelay',
-                        color='Reporting_Airline')                        
+                        color='Reporting_Airline',
+                        title='Average Late Aircraft Delay by Month per Airline')                     
     return [fig1, fig2, fig3, fig4, fig5]
 
 
